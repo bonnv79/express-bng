2a22d3d6ac774dab10b4e430123770816470d7bd
bonnv79 <ngovanbon16@gmail.com>
2022-07-01 16:39:34 +0700

    v1.0.1

--- api/product.js
+++ api/product.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+const dbo = require("../db/connect");
+
 /**
  * GET product list.
  *
@@ -10,12 +12,26 @@ router.get("/", async (req, res) => {
   try {
     res.set('Access-Control-Allow-Origin', '*');
 
-    res.send({
-      status: 200,
-      message: "Get data has successfully",
-      data: [{ id: 1, name: 'hello world' }],
-      version: '1.0.0'
-    });
+    let db_connect = dbo.getDb("react-mongodb-bng-dev");
+    db_connect
+      .collection("records")
+      .find({})
+      .toArray(function (err, result) {
+        if (err) throw err;
+        res.json({
+          status: 200,
+          message: "Get data has successfully",
+          data: result,
+          version: '1.0.1'
+        });
+      });
+
+    // res.send({
+    //   status: 200,
+    //   message: "Get data has successfully",
+    //   data: [{ id: 1, name: 'hello world' }],
+    //   version: '1.0.0'
+    // });
   } catch (error) {
     console.error(error);
     return res.status(500).send("Server error");
--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require("express");
+require("dotenv").config();
+
 const app = express();
+const cors = require("cors");
 const product = require("./api/product");
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json({ extended: false }));
-
 app.use("/api/product", product);
+app.use(cors());
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
+const dbo = require("./db/connect");
+
+app.listen(PORT, () => {
+  dbo.connectToServer(function (err) {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log('Connected Mongodb');
+    }
+  });
+
+  console.log(`Server is running in port ${PORT}`);
+});
