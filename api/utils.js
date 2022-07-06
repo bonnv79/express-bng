5fa934b43f2351bc3b5a7965cec4e28afc225d03
bonnv79 <ngovanbon16@gmail.com>
2022-07-06 14:55:55 +0700

    version v1.0.7

--- api/utils.js
+++ api/utils.js
@@ -1,7 +1,33 @@
+const { connectToDatabase } = require("../db/mongodb");
 const package = require("../package.json");
 
 const getVersion = () => package?.version;
 
+const version = getVersion();
+
+const handleSuccess = (res, { data, ...options }) => {
+  res.send({
+    status: 200,
+    message: "Get data has successfully",
+    version,
+    data,
+    ...options
+  });
+}
+
+const handleError = (res, { error, ...options }) => {
+  console.error(error);
+  return res.status(500).send({
+    status: 500,
+    message: 'Server error',
+    version,
+    error,
+    ...options
+  });
+}
+
 module.exports = {
-  getVersion
+  getVersion,
+  handleSuccess,
+  handleError,
 }
